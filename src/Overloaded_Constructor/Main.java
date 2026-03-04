5fa0bc9d44d7e16fd16086758888c6a91505755b
mohammadjaffarr <mohammadjaffar057@gmail.com>
2026-03-05 03:58:34 +0530

    Update java code of overloaded constructor

--- src/Overloaded_Constructor/Main.java
+++ src/Overloaded_Constructor/Main.java
@@ -1,13 +1,34 @@
 package Overloaded_Constructor;
 
+import java.lang.classfile.attribute.SyntheticAttribute;
+
 public class Main {
     public static void main(String[] args){
 
         // overloaded constructor = Allow a class to have multiple constructors
         //                          with different parameter lists.
         //                          Enable objects to be initialized in various ways.
-
-
         // 07 : 02 : 23
+        User user1 = new User("Ali");
+        User user2 = new User("ALI KHAN","ali24@gmail.com");
+        User user3 = new User("Mohammad ", "Mohammad@gmail.com", 21);
+        User user4 = new User();
+
+        System.out.println(user1.username);
+        System.out.println(user1.email);
+        System.out.println(user1.age);
+
+        System.out.println(user2.username);
+        System.out.println(user2.email);
+        System.out.println(user2.age);
+
+        System.out.println(user3.username);
+        System.out.println(user3.email);
+        System.out.println(user3.age);
+
+        System.out.println(user4.username);
+        System.out.println(user4.email);
+        System.out.println(user4.age);
+        
     }
 }
