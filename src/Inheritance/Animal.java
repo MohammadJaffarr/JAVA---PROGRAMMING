d1339e40d534173f692e779f96ae465ab0d08a2c
Mohammad Jaffarr <mohammdjaffar057@gmail.com>
2026-04-12 23:28:42 +0530

    Understanding Inheritance

---
+++ src/Inheritance/Animal.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class Animal {
+    boolean isAlive;
+    Animal(){
+        isAlive = true;
+    }
+
+    void eat(){
+        System.out.println("The animal is eating");
+    }
+}
---
+++ src/Inheritance/Main.java
@@ -0,0 +1,26 @@
+package Inheritance;
+
+public class Main {
+    public static void main(String[] args){
+
+        // Inheritance = one class inherits the attributes and methods
+        //               from another class.
+        //               Child <- Parent.
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        System.out.println(dog.isAlive);
+        System.out.println(cat.isAlive);
+
+        dog.eat();
+        cat.eat();
+
+        System.out.println(dog.lives);
+        System.out.println(cat.lives);
+
+        dog.speak();
+        cat.speak();
+
+    }
+}
